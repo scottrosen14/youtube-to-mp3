c2f203611f96b7a9f3d73b6c1041d9c12454aea7
scottrosen14 <scott.rosen14@gmail.com>
2022-06-16 17:53:18 -0500

    Call editFileMetaData endpoint within convertToMp3

--- router.js
+++ router.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
 const path = require("path");
+const axios = require('axios');
 const ffmetadata = require("ffmetadata");
 const youtubeDownloader = require('./youtubeMP3-config');
+const {HOST, PORT, PROXY} = require("./server");
 
 const router = Router();
 
 const ffmpegPath = '/usr/local/bin/ffmpeg';
-const outputPath = '/Users/scottrosen/Desktop/Music/Ripped/';
+const outputPath = path.resolve(__dirname, '../../../Music/Ripped');
 const YD = youtubeDownloader(ffmpegPath, outputPath);
 
 const getVideoId = (youtubeLink) => {
@@ -18,50 +20,56 @@ const getVideoId = (youtubeLink) => {
 ffmetadata.setFfmpegPath(ffmpegPath);
 
 router.post('/convertYoutubeToMp3', (req, res) => {
-    const songName = req.body.song + ' - (Ripped)';
-    const fileName = songName + '.mp3';
-    const fullPathName = path.join(outputPath, fileName);
-    YD.download(getVideoId(req.body.youtubeLink), fileName);
+    try {
+        const songName = req.body.song + ' - (Ripped)';
+        const fileName = songName + '.mp3';
+        const fullPathName = path.join(outputPath, fileName);
+        YD.download(getVideoId(req.body.youtubeLink), fileName);
 
-    YD.on("progress", function(progress) {
-        console.log(JSON.stringify(progress));
-    });
-    YD.on("finished", function(err, data) {
-        const editedData = {
-            ...data,
-            title: songName,
-            artist: req.body.artist
-        }
-        ffmetadata.write(fullPathName, editedData, function(err) {
-            if (err) {
-                console.error("Error writing metadata", err);
-                res.status(500).send(JSON.stringify(err));
-            } else {
-                console.log("Edited data written to file" + fileName);
-                res.send(editedData)
+        YD.on("progress", function (progress) {
+            console.log(JSON.stringify(progress));
+        });
+        YD.on("finished", async function (err, data) {
+            try {
+                const editedData = await axios.put(`${HOST}:${PORT}/${PROXY}/editFileMetaData`, {
+                    songName,
+                    artist: req.body.artist,
+                    path: outputPath,
+                    comment: JSON.stringify(req.body.data)
+                })
+                const combinedData = {
+                    ...data,
+                    title: editedData.data.songName,
+                    artist: editedData.data.artist
+                };
+                return res.send(combinedData);
+            } catch (error) {
+                console.log('Error: ' + error);
+                return res.status(500).send(JSON.stringify(error));
             }
         });
-    });
-    YD.on("error", function(error) {
-        console.log(error);
-        res.status(500).send(JSON.stringify(error));
-    });
+    } catch(error) {
+        const err = 'Error with Youtube Downloader: ' + error;
+        console.log(err);
+        res.status(500).send(err);
+    }
 })
 
-router.put('/editFileMetaData', (req, res) => {
+router.put('/editFileMetaData', async (req, res) => {
     const editedData = {
         title: req.body.songName,
         artist: req.body.artist,
-        path: req.body.path
+        path: req.body.path,
+        comment: req.body.comment
     };
     const fileName = path.join(req.body.path, `${req.body.songName}.mp3`);
-    ffmetadata.write(fileName, editedData, function(err) {
+    await ffmetadata.write(fileName, editedData, function(err) {
         if (err) {
             console.error("Error writing metadata", err);
-            res.send(JSON.stringify(err));
+            return res.status(500).send(JSON.stringify(err));
         } else {
-            console.log("Edited data written to file");
-            res.send(editedData)
+            console.log("Edited data written to file: " + fileName);
+            return res.send(editedData);
         }
     });
 })
--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const app = require('express')();
-const path = require('path');
 const bodyParser = require('body-parser');
-const YoutubeMp3Downloader = require("youtube-mp3-downloader");
-const ffmetadata = require("ffmetadata");
 
 const router = require('./router');
 
-app.use(bodyParser.json())
-app.use('/api', router);
-
+const PROXY = '/api';
 const PORT = 3000;
+const HOST = 'http://localhost';
+
+app.use(bodyParser.json())
+app.use(PROXY, router);
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled Rejection at:', reason.stack || reason)
+})
+
 app.listen(PORT, () => {
     console.log('listening on PORT ' + PORT);
 })
+
+module.exports = {
+    PROXY,
+    PORT,
+    HOST
+}
