aae56a10c14d8b3b2a38ea7791288c8b86397d2a
scottrosen14 <scott.rosen14@gmail.com>
2022-07-13 01:39:14 -0500

    Create process files helper

--- router.js
+++ router.js
@@ -1,13 +1,11 @@
 const { Router } = require('express');
 const path = require("path");
 const axios = require('axios');
-const fs = require('fs');
 const ffmetadata = require("ffmetadata");
-const shell = require('shelljs');
 const youtubeDownloader = require('./youtubeMP3-config');
-const {resolve} = require("path");
-axios.defaults.baseURL = 'http://localhost:3000';
+const { readFile, writeMetaData } = require('./tools/processFiles');
 
+axios.defaults.baseURL = 'http://localhost:3000';
 const router = Router();
 
 const ffmpegPath = '/usr/local/bin/ffmpeg';
@@ -21,29 +19,6 @@ const getVideoId = (youtubeLink) => {
 
 // Set path to ffmpeg - optional if in $PATH or $FFMPEG_PATH
 ffmetadata.setFfmpegPath(ffmpegPath);
-
-const readFile = async (filePath) => {
-    return new Promise((resolve, reject) => {
-        fs.stat(filePath, (err, data) => {
-            // Don't reject the error since we want the file to not exist yet
-            resolve(data);
-        })
-    })
-}
-
-const writeMetaData = (fileName, editedData) => {
-    return new Promise((resolve, reject) => {
-        ffmetadata.write(fileName, editedData, function (err, data) {
-            if (err) {
-                console.error("Error writing metadata: ", err);
-                reject(err);
-                return;
-            }
-            console.log("Edited data written to file: " + fileName);
-            resolve(data);
-        })
-    })
-};
 
 router.post('/convertYoutubeToMp3', async (req, res) => {
     try {
